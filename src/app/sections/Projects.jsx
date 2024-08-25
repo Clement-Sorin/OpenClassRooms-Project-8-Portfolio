55510c85557b93ca4ce4ba23fbdd016bc6ef67a9
Clément Sorin <clement.sorin.141@gmail.com>
2024-08-25 14:33:02 +0200

    Projects : create json file with all datas for the portfolio

--- src/app/sections/Projects.jsx
+++ src/app/sections/Projects.jsx
@@ -1,5 +1,6 @@
 import Transition2 from "../components/Transition2"
 import useScrollAnchor from "../hooks/useScrollAnchor"
+import datas from "../../assets/datas/Projects.json"
 
 function Projects() {
     const scrollProjects = useScrollAnchor("transition-2")
@@ -7,10 +8,11 @@ function Projects() {
     return (
         <section id="projects">
             <Transition2 />
-            <div className={`${scrollProjects ? "projects" : "hidden"} sm:top-16 md:top-20 lg:top-24 h-full w-full`}>
-                <h2>TEST</h2>
-
-            </div>
+            <div
+                className={`${
+                    scrollProjects ? "projects" : "hidden"
+                } sm:top-16 md:top-20 lg:top-24 h-full w-full`}
+            ></div>
         </section>
     )
 }
