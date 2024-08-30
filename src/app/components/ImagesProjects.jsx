a1b57bc6cc3717108e53d10a35d4acb3a0bbc83e
Clément Sorin <clement.sorin.141@gmail.com>
2024-08-30 19:16:26 +0200

    ImagesProjects : last update with See More adaptation to the resize and add hover effet on it

--- src/app/components/ImagesProjects.jsx
+++ src/app/components/ImagesProjects.jsx
@@ -8,12 +8,23 @@ function ImagesProjects({ images, title }) {
     const [heightDiv, setHeightDiv] = useState(0)
     const divRoundRef = useRef(null)
 
-    useEffect(() => {
-        if (divRoundRef.current) {
+    const updateHeight = () => {
+        if (divRoundRef.current && !divRoundRef.current <= 0) {
             const heightDivRound = divRoundRef.current.offsetWidth
             setHeightDiv(heightDivRound)
         }
+    }
+
+    useEffect(() => {
+        updateHeight()
+        window.addEventListener("resize", updateHeight)
+
+        return () => {
+            window.removeEventListener("resize", updateHeight)
+        }
     }, [])
+
+    console.log(heightDiv)
 
     return (
         <div className="container-image relative sm:max-w-[250px] sm:max-h-[150px] sm2:max-w-[350px] sm2:max-h-[350px] lg:max-w-[500px] lg:max-h-[500px]">
@@ -30,7 +41,8 @@ function ImagesProjects({ images, title }) {
                     className={`see-more flex flex-col justify-center w-[20%] h-full`}
                 >
                     <button
-                        className={`text-[14px] bg-light-grey w-full h-[${heightDiv}px] rounded-full border border-lines-light`}
+                        className={`text-[10px] sm2:text-[14px] bg-light-grey w-full h-[${heightDiv}px] rounded-full border border-lines-light`}
+                        style={{ height: `${heightDiv}px` }}
                     >
                         {language === "fr" ? "Voir plus" : "See More"}
                     </button>
