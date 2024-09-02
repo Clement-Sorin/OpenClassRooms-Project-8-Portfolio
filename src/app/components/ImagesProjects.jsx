3a2a13001aec0a4fa23145807adff38a6bca26c4
Clément Sorin <clement.sorin.141@gmail.com>
2024-09-02 11:43:24 +0200

    ImagesProjects : change circle into fixed position according to responsive

--- src/app/components/ImagesProjects.jsx
+++ src/app/components/ImagesProjects.jsx
@@ -1,30 +1,9 @@
 import useScrollAnchor from "../hooks/useScrollAnchor"
 import { useAppContext } from "../contexts/AppContext"
-import { useState, useEffect, useRef } from "react"
 
 function ImagesProjects({ images, title }) {
     const scrollSingleProject = useScrollAnchor("transition-2")
     const { theme, language } = useAppContext()
-    const [heightDiv, setHeightDiv] = useState(0)
-    const divRoundRef = useRef(null)
-
-    const updateHeight = () => {
-        if (divRoundRef.current && !divRoundRef.current <= 0) {
-            const heightDivRound = divRoundRef.current.offsetWidth
-            setHeightDiv(heightDivRound)
-        }
-    }
-
-    useEffect(() => {
-        updateHeight()
-        window.addEventListener("resize", updateHeight)
-
-        return () => {
-            window.removeEventListener("resize", updateHeight)
-        }
-    }, [])
-
-    console.log(heightDiv)
 
     return (
         <div className="container-image relative sm:max-w-[250px] sm:max-h-[150px] sm2:max-w-[350px] sm2:max-h-[350px] lg:max-w-[500px] lg:max-h-[500px]">
@@ -34,21 +13,17 @@ function ImagesProjects({ images, title }) {
                 } filter-more absolute z-10 w-full h-full flex justify-center`}
             >
                 <div
-                    className={`more-left border-b-2 w-[40%] h-1/2 border-lines-light border-dotted`}
+                    className={`more-left absolute top-1/2 left-0 right-1/2 sm:mr-[25px] sm2:mr-[40px] lg:mr-[50px] border-b-2 border-lines-light border-dotted`}
                 ></div>
-                <div
-                    ref={divRoundRef}
-                    className={`see-more flex flex-col justify-center w-[20%] h-full`}
-                >
+                <div className={`see-more flex flex-col justify-center h-full`}>
                     <button
-                        className={`text-[10px] sm2:text-[14px] bg-light-grey w-full h-[${heightDiv}px] rounded-full border border-lines-light`}
-                        style={{ height: `${heightDiv}px` }}
+                        className={`btn-see-more text-[10px] sm2:text-[14px] bg-light-grey sm:w-[50px] sm:h-[50px] sm2:w-[80px] sm2:h-[80px] lg:w-[100px] lg:h-[100px] rounded-full border border-lines-light`}
                     >
                         {language === "fr" ? "Voir plus" : "See More"}
                     </button>
                 </div>
                 <div
-                    className={`more-right border-b-2 w-[40%] h-1/2 border-lines-light border-dotted`}
+                    className={`more-right absolute top-1/2 right-0 left-1/2 sm:ml-[25px] sm2:ml-[40px] lg:ml-[50px] border-b-2  border-lines-light border-dotted`}
                 ></div>
             </div>
             <img
