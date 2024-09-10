5f22d629881f4c0c72d638a4876b70b29f51e392
Clément Sorin <clement.sorin.141@gmail.com>
2024-09-10 15:10:51 +0200

    Contact : adapt to responsive

--- src/app/sections/Contact.jsx
+++ src/app/sections/Contact.jsx
@@ -2,10 +2,12 @@ import { useAppContext } from "../contexts/AppContext"
 import { ReactComponent as FrameName } from "../../assets/svgs/frame-contact-name.svg"
 import { ReactComponent as FrameMessage } from "../../assets/svgs/frame-contact-message.svg"
 import { ReactComponent as FrameSubmit } from "../../assets/svgs/frame-submit.svg"
+import { useRef } from "react"
 import datas from "../../assets/datas/Contact.json"
 
 function Contact() {
     const { theme, language } = useAppContext()
+    const formRef = useRef(null)
 
     return (
         <section
@@ -15,22 +17,26 @@ function Contact() {
             }`}
         >
             <div className="container-contact min-h-[80vh] w-full flex justify-center items-center pt-24">
-                <form action="/submit" method="post">
+                <form ref={formRef} action="/submit" method="post">
                     <div
-                        className={`flex flex-col gap-5 ${
+                        className={`container-form flex flex-col gap-5 ${
                             theme === "light" ? "text-black" : "text-dark-text"
                         }`}
                     >
                         <div className="contact-name flex justify-between">
-                            <label for="input-name" className="text-xl">
+                            <label
+                                for="input-name"
+                                className="label sm:text-sm sm2:text-lg md:text-xl"
+                            >
                                 {language === "fr"
                                     ? datas.name.fr
                                     : datas.name.en}
                             </label>
-                            <div className="relative w-[500px] flex justify-start">
+                            <div className="input-name relative w-[500px] flex justify-start">
                                 <input
                                     type="text"
                                     id="input-name"
+                                    required
                                     className="absolute w-[302px] h-[52px] bg-transparent outline-none p-2"
                                     aria-label="name input field"
                                     aria-required="true"
@@ -43,19 +49,24 @@ function Contact() {
                                             ? "#757780"
                                             : "#E7DAE0"
                                     }
+                                    className="frame-contact-name"
                                 />
                             </div>
                         </div>
                         <div className="contact-email flex justify-between">
-                            <label for="input-email" className="text-xl">
+                            <label
+                                for="input-email"
+                                className="label sm:text-sm sm2:text-lg md:text-xl"
+                            >
                                 {language === "fr"
                                     ? datas.email.fr
                                     : datas.email.en}
                             </label>
-                            <div className="relative w-[500px] flex justify-start">
+                            <div className="input-name relative w-[500px] flex justify-start">
                                 <input
                                     type="email"
                                     id="input-email"
+                                    required
                                     className="absolute w-[302px] h-[52px] bg-transparent outline-none p-2"
                                     aria-label="email input field"
                                     aria-required="true"
@@ -68,19 +79,24 @@ function Contact() {
                                             ? "#757780"
                                             : "#E7DAE0"
                                     }
+                                    className="frame-contact-name"
                                 />
                             </div>
                         </div>
                         <div className="contact-message flex justify-between gap-10">
-                            <label for="area-message" className="text-xl">
+                            <label
+                                for="area-message"
+                                className="label sm:text-sm sm2:text-lg md:text-xl"
+                            >
                                 {language === "fr"
                                     ? datas.message.fr
                                     : datas.message.en}
                             </label>
-                            <div className="relative w-[500px] flex justify-start">
+                            <div className="container-message relative w-[500px] flex justify-start">
                                 <textarea
                                     type="text"
                                     id="area-message"
+                                    required
                                     className="absolute w-[499px] h-[182px] bg-transparent outline-none p-2 resize-none"
                                     aria-label="message input field"
                                     aria-required="true"
@@ -93,6 +109,7 @@ function Contact() {
                                             ? "#757780"
                                             : "#E7DAE0"
                                     }
+                                    className="frame-contact-message"
                                 />
                             </div>
                         </div>
@@ -105,7 +122,7 @@ function Contact() {
                                         : datas.submit.en
                                 }
                                 aria-label="send message form"
-                                className="input-submit absolute w-[124px] h-[36px]"
+                                className="input-submit sm:text-xs sm2:text-sm md:text-xl absolute w-[124px] h-[36px]"
                             ></input>
                             <FrameSubmit
                                 className="frame-submit"
